27ab00975039719d779ba8b1245664f5ad687f96
SabugoR <SabugoR@users.noreply.github.com>
2018-09-30 20:32:47 +0300

    some controllers added

---
+++ Assets/BunnyController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunnyController : MonoBehaviour {
+    private Transform player;
+    // Use this for initialization
+    void Start () {
+		player  = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<UnityEngine.AI.NavMeshAgent>().SetDestination(player.position);
+        GetComponent<Animator>().SetBool("IsWalking", GetComponent<UnityEngine.AI.NavMeshAgent>().velocity.magnitude > 0 );
+
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 10.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 10.0f;
+        Vector3 movement = new Vector3(x, 0f, z);
+            GetComponent<Rigidbody>().MovePosition(transform.position + movement);
+        Vector3 worldpos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y, 7));
+        transform.LookAt(new Vector3(worldpos.x, transform.position.y, worldpos.z));
+        GetComponent<Animator>().SetBool("IsWalking", x != 0f || z != 0f);
+
+
+    }
+}
